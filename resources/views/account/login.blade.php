973f5ce6dd334b77af02e7ae85df82ef5be6035b
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2024-07-25 17:40:10 +0530

    fix conflict

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -10,52 +10,52 @@
                 <div class="alert alert-success">
                     {{ Session::get('success') }}
                 </div>
-                
-                        @endif
-                        
-                        @if(Session::has('error'))
-                            <div class="alert alert-danger">
-                                {{ Session::get('error') }}
-                            </div>
-                        @endif
 
-                        <div class="card border border-light-subtle rounded-4">
-                            <div class="card-body p-3 p-md-4 p-xl-5">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="mb-5">
-                                            <h4 class="text-center">Login Here</h4>
-                                        </div>
+                @endif
+
+                @if(Session::has('error'))
+                <div class="alert alert-danger">
+                    {{ Session::get('error') }}
+                </div>
+                @endif
+
+                <div class="card border border-light-subtle rounded-4">
+                    <div class="card-body p-3 p-md-4 p-xl-5">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="mb-5">
+                                    <h4 class="text-center">Login Here</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <form action="{{ route('account.authenticate') }}" method="POST">
+                            @csrf
+                            <div class="row gy-3 overflow-hidden">
+                                <div class="col-12">
+                                    <div class="form-floating mb-3">
+                                        <input type="text" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="name@example.com">
+                                        <label for="email" class="form-label">Email</label>
+                                        @error('email')
+                                        <p class="invalid-feedback">{{ $message }}</p>
+                                        @enderror
                                     </div>
                                 </div>
-                                <form action="{{ route('account.authenticate') }}" method="POST">
-                                    @csrf
-                                    <div class="row gy-3 overflow-hidden">
-                                        <div class="col-12">
-                                            <div class="form-floating mb-3">
-                                                <input type="text" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="name@example.com">
-                                                <label for="email" class="form-label">Email</label>
-                                                @error('email') 
-                                                    <p class = "invalid-feedback">{{ $message }}</p>
-                                                @enderror 
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="form-floating mb-3">
-                                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" value="" placeholder="Password">
-                                                <label for="password" class="form-label">Password</label>
-                                                @error('password') 
-                                                    <p class = "invalid-feedback">{{ $message }}</p>
-                                                @enderror 
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="d-grid">
-                                                <button class="btn bsb-btn-xl btn-primary py-3" type="submit">Log In Now</button>
-                                            </div>
-                                        </div>
+                                <div class="col-12">
+                                    <div class="form-floating mb-3">
+                                        <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" value="" placeholder="Password">
+                                        <label for="password" class="form-label">Password</label>
+                                        @error('password')
+                                        <p class="invalid-feedback">{{ $message }}</p>
+                                        @enderror
                                     </div>
                                 </div>
+                                <div class="col-12">
+                                    <div class="d-grid">
+                                        <button class="btn bsb-btn-xl btn-primary py-3" type="submit">Log In Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
                                 <div class="col-12">
                                     <div class="d-grid">
                                         <button class="btn bsb-btn-xl btn-primary py-3" type="submit">Log In Now</button>
