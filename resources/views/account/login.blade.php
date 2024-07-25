6d6666ce9e784f53a892c82fe53e2b9c3ba880c5
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2024-07-25 17:27:45 +0530

    implemented login authentication system

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -28,6 +28,13 @@
                                 {{ Session::get('success') }}
                             </div>
                         @endif
+                        
+                        @if(Session::has('error'))
+                            <div class="alert alert-danger">
+                                {{ Session::get('error') }}
+                            </div>
+                        @endif
+
                         <div class="card border border-light-subtle rounded-4">
                             <div class="card-body p-3 p-md-4 p-xl-5">
                                 <div class="row">
@@ -37,18 +44,25 @@
                                         </div>
                                     </div>
                                 </div>
-                                <form action="profile.html">
+                                <form action="{{ route('account.authenticate') }}" method="POST">
+                                    @csrf
                                     <div class="row gy-3 overflow-hidden">
                                         <div class="col-12">
                                             <div class="form-floating mb-3">
-                                                <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+                                                <input type="text" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="name@example.com">
                                                 <label for="email" class="form-label">Email</label>
+                                                @error('email') 
+                                                    <p class = "invalid-feedback">{{ $message }}</p>
+                                                @enderror 
                                             </div>
                                         </div>
                                         <div class="col-12">
                                             <div class="form-floating mb-3">
-                                                <input type="password" class="form-control" name="password" id="password" value="" placeholder="Password">
+                                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" value="" placeholder="Password">
                                                 <label for="password" class="form-label">Password</label>
+                                                @error('password') 
+                                                    <p class = "invalid-feedback">{{ $message }}</p>
+                                                @enderror 
                                             </div>
                                         </div>
                                         <div class="col-12">
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('account/register', [AccountController::class, 'register'])->name('account.register');
-Route::post('account/register', [AccountController::class, 'processRegister'])->name('account.processRegister');
 
-Route::get('account/login', [AccountController::class, 'login'])->name('account.login');
+// Make routes guests and authenticated that means routes are safe from directly accessible by any user
+// only logged in users can access auth routes
+// non logged users redirected to the login route
+Route::group(['prefix' => 'account'], function () {
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('register', [AccountController::class, 'register'])->name('account.register');
+        Route::post('register', [AccountController::class, 'processRegister'])->name('account.processRegister');
+        Route::get('login', [AccountController::class, 'login'])->name('account.login');
+        Route::post('login', [AccountController::class, 'authenticate'])->name('account.authenticate');
+    });
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('profile', [AccountController::class, 'profile'])->name('account.profile');
+    });
+});
