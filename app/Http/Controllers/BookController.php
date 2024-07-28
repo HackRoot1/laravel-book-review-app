ed71c5d92bd9b5ab518a6770070e5f3d7bb05846
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2024-07-28 15:59:55 +0530

    book adding and searching and pagination added

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+
+class BookController extends Controller
+{
+    // This method will show books listing page
+    public function index(Request $request) {
+        
+        $books = Book::orderBy('created_at', 'DESC');
+
+        if(!empty($request->keyword)) {
+            $books->where('title', 'like', '%'.$request->keyword.'%');
+        }
+
+        $books = $books->paginate(10);
+
+        return view('books.list', [
+            'books' => $books
+        ]);
+    }
+
+    // This method will show create book page 
+    public function create() {
+        return view('books.create');
+    }
+    
+    // This method will a book in database 
+    public function store(Request $request) {
+
+        $rules = [
+            'title' => 'required|min:5',
+            'author' => 'required|min:3',
+            'status' => 'required',
+        ];
+
+        if(!empty($request->image)) {
+            $rules['image'] = 'image';
+        }
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            return redirect()->route('books.create')->withInput()->withErrors($validator);
+        }
+
+        // save book in database 
+        $book = new Book();
+        $book->title = $request->title;
+        $book->description = $request->description;
+        $book->author = $request->author;
+        $book->status = $request->status;
+        $book->save();
+
+        // upload book here
+        if(!empty($request->image)) {
+            // $image = $request->file('image');
+            
+            $image = $request->image;
+            $ext = $image->getClientOriginalExtension();
+            $imageName = time() . "." . $ext;
+            $image->move(public_path('uploads/books'), $imageName);
+            
+            $book->image = $imageName;
+            $book->save();
+
+            $manager = new ImageManager(Driver::class);
+            $img = $manager->read(public_path('uploads/books/'. $imageName));
+            $img->resize(990);
+            $img->save(public_path('uploads/books/thumb/'.$imageName));
+        }
+
+        return redirect()->route('books.index')->with('success', 'Book added successfully.');
+
+    }
+    
+    // This method will show edit book page
+    public function edit() {
+
+    }
+    
+    // This method will update a book
+    public function update() {
+
+    }
+    
+    // This method will delete a book from database
+    public function destroy() {
+
+    }
+}
--- database/migrations/2024_07_28_090024_create_books_table.php
+++ database/migrations/2024_07_28_090024_create_books_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('status')->default(1);
             $table->timestamps();
         });
     }
