9b8297c71d34c0729a6296c740f11ac1dc7e61d6
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2024-07-28 14:31:00 +0530

    profile update logics added

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,18 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class AccountController extends Controller
 {
     // This method will show register page
-    public function register() {
+    public function register()
+    {
         return view('account.register');
     }
 
     // This method will register a user
-    public function processRegister(Request $request) {
+    public function processRegister(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required|min:3',
             'email' => 'required|email|unique:users',
@@ -24,7 +29,7 @@ class AccountController extends Controller
             'password_confirmation' => 'required',
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return redirect()->route('account.register')->withInput()->withErrors($validator);
         }
 
@@ -38,21 +43,23 @@ class AccountController extends Controller
         return redirect()->route('account.login')->with('success', 'You have registered successfully.');
     }
 
-    public function login() {
+    public function login()
+    {
         return view('account.login');
     }
 
-    public function authenticate(Request $request) {
+    public function authenticate(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return redirect()->route('account.login')->withInput()->withErrors($validator);
         }
 
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             return redirect()->route('account.profile');
         } else {
             return redirect()->route('account.login')->with('error', 'Either email/password is incorrect.');
@@ -60,31 +67,61 @@ class AccountController extends Controller
     }
 
     // This method will show profile page.
-    public function profile() {
+    public function profile()
+    {
         $user = User::find(Auth::user()->id);
         return view('account.profile', ['user' => $user]);
     }
 
     // This method will update profile page.
-    public function updateProfile(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|min:3',
-            'email' => 'required|email|unique:users, email, '. Auth::user()->id . ', id',
-        ]);
+    public function updateProfile(Request $request)
+    {
 
-        if($validator->fails()) {
+        $rules = [
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users,email,'.Auth::user()->id.',id',
+        ];
+
+        if (!empty($request->image)) {
+            $rules['image'] = 'image|mimes:jpeg,png,jpg,gif,svg|max:2048';
+        }
+        
+        $validator = Validator::make($request->all(), $rules);
+        
+        if ($validator->fails()) {
             return redirect()->route('account.profile')->withInput()->withErrors($validator);
         }
 
         $user = User::find(Auth::user()->id);
         $user->name = $request->name;
         $user->email = $request->email;
+
+        // Here we will upload image
+
+        if (!empty($request->image)) {
+
+            // Delete old image
+            File::delete(public_path('uploads/profile/'. $user->image));
+            File::delete(public_path('uploads/profile/thumb/'. $user->image));
+
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('uploads/profile'), $imageName);
+            $user->image = $imageName;
+
+            $manager = new ImageManager(Driver::class);
+            $img = $manager->read(public_path('uploads/profile/'. $imageName));
+            $img->cover(150, 150);
+            $img->save(public_path('uploads/profile/thumb/'.$imageName));
+        }
+
         $user->save();
 
         return redirect()->route('account.profile')->with('success', 'Profile updated successfully.');
     }
 
-    public function logout() {
+    public function logout()
+    {
         Auth::logout();
         return redirect()->route('account.login');
     }
